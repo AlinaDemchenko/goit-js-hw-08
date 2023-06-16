d8dba2051e95d22a3c9af88265893afd03a634fe
Alina Demchenko <demalina88@gmail.com>
2023-06-15 19:55:25 -0700

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function(){});
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);});
+
+const onPlay = function({seconds}) {
+            localStorage.setItem("videoplayer-current-time",seconds)};
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+        }).catch(function(error) {switch (error.name) {case 'RangeError': break; default: break; }});
+    
+player.on('timeupdate', throttle(onPlay, 1000));
+
+console.log(Player);
+console.log(throttle);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    button: document.querySelector(".submit")
+}
+
+const email = refs.form.elements.email;
+const message = refs.form.elements.message;
+
+function autocomplete (){
+    const storageData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+    email.value = storageData.email || "";
+    message.value = storageData.message || "";
+}
+
+autocomplete()
+
+function handlerForm (evt){
+const userData = {};
+userData.email = email.value || "";
+userData.message = message.value || "";
+localStorage.setItem("feedback-form-state", JSON.stringify(userData))
+}
+
+function resetHandler (evt){
+    evt.preventDefault(); 
+    console.log(localStorage.getItem("feedback-form-state"));
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state")
+}
+
+refs.form.addEventListener("input", throttle(handlerForm, 500))
+refs.form.addEventListener("submit", resetHandler)
